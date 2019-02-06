d3a9ff146828c337924c9463e43a1d58ae30f17c
btjd <btjd@users.noreply.github.com>
2019-02-05 23:39:05 -0500

    added one_edit_distance and updated increment_list with a better iterative implementation

--- increment_list.py
+++ increment_list.py
@@ -23,29 +23,16 @@ def _rec_list_sum(arr, i):
     #     return arr
 
 
-def incremet_iter(arr):
-    carry = 0
-    last = len(arr) - 1
-    for i in range(last, -1, -1):
-        if i == last and arr[i] == 9:
-            arr[i] = 0
-            carry = 1
-        elif i == last and arr[i] != 9:
-            arr[i] += 1
-        elif i > 0 and carry == 1:
-            if arr[i] != 9:
-                arr[i] += 1
-                carry = 0
-            else:
-                arr[i] = 0
-                carry = 1
-        elif i == 0 and carry == 1:
-            if arr[i] == 9:
-                arr[i] = 0
-                arr.insert(0, 1)
-            else:
-                arr[i] += 1
-    return arr
+def incremet_iter(digits):
+    for i in range(len(digits)-1,-1,-1):
+        if i>=0 and digits[i]<9:
+            digits[i] += 1
+            return digits
+        elif i>0 and digits[i]==9:
+            digits[i] = 0
+        else:
+            digits[i]=0
+            return [1]+digits
 
 
 arr = [1, 3, 5]
